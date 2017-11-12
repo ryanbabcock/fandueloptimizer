c19cb2675b0a46ee4aa209ba88d13505250a94a9
babslabs <ryanbruce99@gmail.com>
2017-11-12 17:53:13 -0500

    this doesnt work

--- FanduelOptimizer.java
+++ FanduelOptimizer.java
@@ -8,8 +8,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class FanduelOptimizer { 
 	//public static ArrayList importedPlayers = new ArrayList();
@@ -22,18 +24,25 @@ public class FanduelOptimizer {
 		List<Player> printingPlayers = importCSVFile("fanduel.csv");
 
 		// let's print all the person read from CSV file
+		/*
 		for (Player i : printingPlayers) {
 			System.out.println(i);
 		}
+		*/
 	}
-	private static Player createPlayer(int playerId, String[] playerData){
+	private static Player createPlayer(int playerId, ArrayList<String[]> csvdata){
 		int id = playerId;
-		boolean injured = Boolean.parseBoolean(playerData[11]);
-		String position = playerData[1];
-		int salary = Integer.parseInt(playerData[7]);
-		String name = playerData[3];
-		String team = playerData[9];
-		return new Player(id, injured, position, salary, name, team);
+		boolean injured = false;
+		if (csvdata.size() >= 11){
+			injured = true;
+		}
+System.out.println(csvdata.get(1));
+		//String position = csvdata.get(1);
+		//int salary = Integer.parseInt(csvdata.get(7));
+		//String name = csvdata.get(3);
+		//String team = csvdata.get(9);
+		//return new Player(id, injured, position, salary, name, team);
+		return new Player(1,true,"test",66,"test","test");
 	}
 	private static List<Player> importCSVFile(String fileName){
 		List<Player> importedPlayers = new ArrayList<>();
@@ -50,17 +59,23 @@ public class FanduelOptimizer {
 		//begin with first player id = 0
 		int id = 0;
 		//gets rid of line 0
+		ArrayList<String[]> playerData = new ArrayList<>();
 		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
 			// read the first line from the text file
-			String line = br.readLine();
+			String line = "";
 			// loop until all lines are read
-			while (line != null) {
+			while ((line = br.readLine()) != null) {
 				// use string.split to load a string array with the values from each line of the file, using a comma as the delimiter
-				String[] playerData = line.split(",");
+				playerData.add(line.split(","));
+//debug System.out.print(line + "\n\n\nffff");
+				//debug System.out.print(playerData);
 				Player importedPlayer = createPlayer(id, playerData);
 				//createPlayer() takes an array list and id, Player() takes the values.
 				// adding book into ArrayList
+//debug importedPlayer.printPlayer();
 				importedPlayers.add(importedPlayer);
+
+				
 				//increment id for next player
 				++id;
 				// read next line before looping
